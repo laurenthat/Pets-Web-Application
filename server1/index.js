fc73c613dcd0500330494d87dc8e923cb1fb278e
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2022-10-26 15:17:33 +0300

    worked with pug

--- server1/index.js
+++ server1/index.js
@@ -8,12 +8,14 @@ let requestCounter = 0;
 
 //this shows all contents of public folder in the url address
 app.use(express.static('public'));
+app.set('view engine', 'pug');
 
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // });
 
+//this requests a JSON
 app.get('/catinfo', (req, res) => {
     const cat = {
         name: "Frank Catman",
@@ -26,7 +28,17 @@ app.get('/catinfo', (req, res) => {
 app.get('/test', (req, res) => {
     console.log('someone is trying to test me.');
     requestCounter++;
-    res.send('<h1>TEST page</h1><p>' + requestCounter + '<p>');
+
+    //example of using pug
+    res.render('test', {
+        title: "Pug test page",
+        header1: "Pug test page",
+        header2: "Counter",
+        exampleText: "Page requested" + requestCounter + " times.",
+    });
+
+    //basic html as string
+    //res.send('<h1>TEST page</h1><p>' + requestCounter + '<p>');
 });
 
 app.listen(port, () => {
