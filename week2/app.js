eb6565b9ba3f45542f043afdb3f44e164a8bbf8c
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2022-11-01 14:18:55 +0200

    added week2 files

--- week2/app.js
+++ week2/app.js
@@ -7,6 +7,14 @@ app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
 
+app.post('/cat', (req, res)=> {
+  console.log(req)
+  res.send('From this endpoint or location you can add more cats.')
+});
+
+app.put('TODO');
+app.delete(TODO);
+
 app.get('/user', (req, res) => {
   res.send('From this endpoint you can get users.')
 });
